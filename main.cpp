a3a61c28fa79f9c0c2d9231aa3ec392f12b9d560
Martin Keegan <martin@no.ucant.org>
2014-11-15 17:31:52 +0000

    remove commented lines

--- main.cpp
+++ main.cpp
@@ -7,11 +7,6 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QString filename = a.arguments().first();
-//    QString filename = "/home/matt/hackuna/OSM/congo-democratic-republic-latest.osm";
-//    QString filename = "/home/matt/hackuna/OSM/TheHague.osm";
-//    QString filename = "/home/matt/hackuna/OSM/Kigoma.osm";
-//    QString filename = "/home/matt/hackuna/OSM/Uvinza.osm";
-//    QString filename = "/home/matt/hackuna/OSM/Mpanda.osm";
     qDebug() << "About to process" << filename;
     RoadFinder parser(filename, true);
     HistoryLookup lookup(true);
